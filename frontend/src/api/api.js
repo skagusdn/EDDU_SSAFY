b9e34c12595c5ed213831665b0811d574e546aa0
moochang <sodauschlrdh@naver.com>
2022-08-03 14:36:06 +0900

    문제 생성하기
    Former-commit-id: bf64ebb83b47018b5426f63ede030aa9663a3516

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,10 +1,12 @@
 const HOST = "http://localhost:8080/api/v1/";
 
 const USERS = "users/";
+const QUIZ = "quiz/";
 
 const users = {
   signup: () => HOST + USERS + "signup/",
   login: () => HOST + USERS + "login/",
   me: () => HOST + USERS + "me/",
+  createQuiz: () => HOST + QUIZ + "createQuiz/"
 };
 export default users;
