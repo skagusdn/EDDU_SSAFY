7c9265b7caecfe9a155cc828aada9a2a98f81172
frozen_load7 <frozen_load7@naver.com>
2022-08-04 14:32:15 +0900

    문제집 불러오기
    Former-commit-id: 3129cdbc8cdd161687df88ce3830cca2a5b8bf55

--- backend-java/src/main/java/com/ssafy/api/service/UserQuizbookService.java
+++ backend-java/src/main/java/com/ssafy/api/service/UserQuizbookService.java
@@ -1,9 +1,12 @@
 package com.ssafy.api.service;
 
 import com.ssafy.api.request.UserQuizbookReq;
+import com.ssafy.api.response.QuizbooksOfUserRes;
 
 public interface UserQuizbookService {
     public Boolean register(UserQuizbookReq userQuizbookReq);
 
     public boolean delete(UserQuizbookReq userQuizbookReq);
+
+    QuizbooksOfUserRes getQuizbookCombsByUserId(String userId);
 }
--- backend-java/src/main/java/com/ssafy/api/service/UserQuizbookServiceImpl.java
+++ backend-java/src/main/java/com/ssafy/api/service/UserQuizbookServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6e4fa59a7cd34614edfdaa5008013e5d60b913d8ad47a0283f703f9e13ee1a0
-size 1193
+oid sha256:c63a6d96002ff11b074292ccaca3a8db94e5b7ae20427c2abc7a873ff038f360
+size 2403
