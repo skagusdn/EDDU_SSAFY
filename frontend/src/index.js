062a35e78abab796aa53c95cd8000204d60fe166
최영선 <vnddns26@naver.com>
2022-08-08 11:11:39 +0900

    css 추가
    Former-commit-id: c9e2db3b7a701df34f6cdac954d49eb9855c05e7

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae0e7eab87b8c9dc7e03b1170a6a6afb118841cca195b69599dd4b0b8435688c
-size 606
+oid sha256:f397d6facad861cc15fecfba3c4637d3793283ff867cd670bfdc4d9710b0fe05
+size 605
