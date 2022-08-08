14a15e2193f5aca9424877734bdc448254b6291c
최영선 <vnddns26@naver.com>
2022-08-08 13:50:24 +0900

    openvidu 수정
    Former-commit-id: f527f16e9cf989701965b71395c1d182d4ed319f

--- frontend/src/pages/Openvidu.js
+++ frontend/src/pages/Openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fb43d37c14cc6e9b8a06f03aa8964304f9507eccec6bf758935c4afeb7b7a34
-size 6316
+oid sha256:b5b2f86dec4aa485b968d9afa07e3f034d42fcd1fe440917a320f0e6e219ee04
+size 6326
