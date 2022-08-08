bea9a00866a6fd9bce58f3357dfd47439da1cbcb
최영선 <vnddns26@naver.com>
2022-08-08 13:27:50 +0900

    react openvidu
    Former-commit-id: ee0a3c5a46d594afc59297f25ce0932a0b61e4d4

--- frontend/src/pages/Openvidu.js
+++ frontend/src/pages/Openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64411325900d90052ba5edd1060cec161197019cb10e0aa6f7478fe466f65fbc
-size 14335
+oid sha256:5fb43d37c14cc6e9b8a06f03aa8964304f9507eccec6bf758935c4afeb7b7a34
+size 6316
