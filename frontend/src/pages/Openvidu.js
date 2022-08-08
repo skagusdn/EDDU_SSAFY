8a5a13a47b73f769c742e7243fcb873e91cc7d83
최영선 <vnddns26@naver.com>
2022-08-08 11:13:12 +0900

    css 수정
    Former-commit-id: b1ae42ad618ad41cf43d8740fe9fc8a3188e1c8a

--- frontend/src/pages/Openvidu.js
+++ frontend/src/pages/Openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81f84a8fd27f60ea98bcd01fd593edeeb7f2bf7a7845fa7f4629e6cf41b76ddb
-size 14365
+oid sha256:64411325900d90052ba5edd1060cec161197019cb10e0aa6f7478fe466f65fbc
+size 14335
