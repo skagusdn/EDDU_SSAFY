eb4788be5601270be3abcf84d6717950118759d7
최영선 <vnddns26@naver.com>
2022-08-08 11:16:09 +0900

    api 수정
    Former-commit-id: c58c6042f5bdc286e577c7e93c1c887a9a98bd46

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:619150cf814eb16f52121c0f82b256f06c803e508831a0cdef6a9f8b6cd22b73
-size 520
+oid sha256:03b25d490238b1df8b332797f24482c751fdbf4780a0b0ad2a4ba4727f334a50
+size 530
