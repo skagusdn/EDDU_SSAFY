94d2b243f4774bb94d58de3588928136b6d5dabb
최영선 <vnddns26@naver.com>
2022-08-09 11:02:55 +0900

    페이지 이동 원할
    Former-commit-id: 10932bc296ec6100a84b7c036ac48a816ca06d69

--- frontend/src/pages/CreateQuestion.js
+++ frontend/src/pages/CreateQuestion.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5dde5134886105e8944f8037b75473e1d58b4b98286ef408b1f201b646026c20
-size 6163
+oid sha256:41b7d214ec11feda294eaaded614f56c3c0db47095477a814e5eedae0569598d
+size 6161
--- frontend/src/pages/ProblemList.js
+++ frontend/src/pages/ProblemList.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5a18e1cd0ddca842d9f11d6290a8deb56fe57b5e3c7b01152d96eac7a5bc525
-size 13711
+oid sha256:f07a4f9379cb58e8a116a42de7a67852544c9704aa0caf22d65ba6ad93b737b7
+size 13790
