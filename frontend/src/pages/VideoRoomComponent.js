32dc356d2fd10b5b7b81a8f260cf4a770de5d3c3
최영선 <vnddns26@naver.com>
2022-08-11 16:20:01 +0900

    서버 배포
    Former-commit-id: 017d4a6b05be2b48aacc2802a69b3e18159aea58

--- frontend/src/pages/VideoRoomComponent.js
+++ frontend/src/pages/VideoRoomComponent.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfc009c6565b6d1b2e317e37854398cdb8b041e5f7386acf723979017c00d73f
-size 23735
+oid sha256:30d2b41d7af2650fe87a21f30192b8755f1c208610bfc16891d88814eede2f8c
+size 23716
