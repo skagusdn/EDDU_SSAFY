3c064bfeaec3439904022e349dc49aa9f19c13cb
최영선 <vnddns26@naver.com>
2022-08-12 15:02:23 +0900

    수정
    Former-commit-id: e51df843f28c599113e838a88aa09d57d09cda44

--- frontend/src/pages/homepage/RoomList.js
+++ frontend/src/pages/homepage/RoomList.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4aba97e1441b3b6ed8a65debf1b17399ff96ab00a86d2c31951ccc96d31eb6ad
-size 2810
+oid sha256:8ccdc082646febd820b4d7cb42aad9e010026ac1ef36f36d8419d48460cc8655
+size 2818
