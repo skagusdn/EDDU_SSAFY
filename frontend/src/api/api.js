050665ac8dfaee64de67682588712665c045b071
Kim-Ji-soo <67624305+Kim-Ji-soo@users.noreply.github.com>
2022-08-13 19:18:25 +0900

    local로 설정변경
    Former-commit-id: 4f6ba360252a9563a40df6acc6239c8334d08d9f

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31f69aef307dcf0d88b585e426e14d37ce1616d47d0a36f51aa72c462b6793de
-size 1123
+oid sha256:438f5e1e8808ed579382620d37768eceb40a4487370398f8b94e2f4f34adc818
+size 1171
