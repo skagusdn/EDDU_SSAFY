01387a634240fbaaf4d6bf18da0403c36df3c3b1
최영선 <vnddns26@naver.com>
2022-08-13 15:40:52 +0900

    버그 수정
    Former-commit-id: 08d24b8103d38157fc8e1a86e842e7a8ba42fea1

--- frontend/src/components/quiz/QuizComponent.js
+++ frontend/src/components/quiz/QuizComponent.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cced7d4611ddeddda506094a47d9eef5c5d5b6e3e267a54a8b6429a7a75bcb06
-size 13111
+oid sha256:34b71156a5233687d6ef6fe7f4a251f1d926f748ab172c7d15ed28b995697a6f
+size 13092
--- frontend/src/pages/VideoRoomComponent.js
+++ frontend/src/pages/VideoRoomComponent.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8d3b42b1ad9663eb056eeb4a220585d365278338d8ae234bf68a67de6b091e6
-size 25228
+oid sha256:f51a8d59deaf433905e06e1be8f5525b982276b7690fbcf80c57e594bae8e579
+size 25294
