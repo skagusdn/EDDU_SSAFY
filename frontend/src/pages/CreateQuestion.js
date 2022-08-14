f72a0f56552a0998d909b3da22e4b1afeb7c0d9c
Kim-Ji-soo <67624305+Kim-Ji-soo@users.noreply.github.com>
2022-08-14 12:12:20 +0900

    quizPic 수정
    Former-commit-id: 78bb435a9491afe6a45d3f45979d58166b3cd7f5

--- frontend/src/pages/CreateQuestion.js
+++ frontend/src/pages/CreateQuestion.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51f9025e3663fdd3ecf3414740d127e8181fc6502e03dbc66d8db9979277f503
-size 7153
+oid sha256:e9c593f7b9ab9d770ba53ca7c99cf74f410a9a7784c4bfc80f6b4faf1ff566f6
+size 7148
