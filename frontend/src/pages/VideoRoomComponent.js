ab431c7288f17e46bf4aa2c413475da3281ab44a
moochang <sodauschlrdh@naver.com>
2022-08-15 21:04:01 +0900

    scroll 적용과 message design 수정
    Former-commit-id: 9069f965167d4c81cb4a20a1b4a79a46702b3c54

--- frontend/src/pages/VideoRoomComponent.js
+++ frontend/src/pages/VideoRoomComponent.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a90483270bb329780f0e5479534e2d3da74c9e0448957a843b3943831dd0ac06
-size 24259
+oid sha256:229a3f1c1edaf2c3b0f17a617a84f320d44fd2be3830dbb6a4256fdada98ddac
+size 24444
