961009382b4dc083a0932fe2c3987f794f19b179
Kim-Ji-soo <67624305+Kim-Ji-soo@users.noreply.github.com>
2022-08-16 20:52:00 +0900

    방 비밀번호 기능 구현
    Former-commit-id: 03d148868f2287f9bbafdf41887a0e9fba1498a9

--- frontend/src/pages/homepage/RoomList.js
+++ frontend/src/pages/homepage/RoomList.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09d78aee1cf01a72f0e43557317f09f36e01c02a82f7f71cf932d1718c11014f
-size 6297
+oid sha256:f64edbba0350828341c3a386fe0ca9354a2d53fec783f4a61c421e0b8a7630e0
+size 6612
