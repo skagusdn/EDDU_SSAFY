f1fea85332adda3f4de28b66a4f873255db504b7
ksmhero <ksmhero@naver.com>
2022-08-16 15:31:31 +0900

    mainpage design
    Former-commit-id: 9a7d1497d387552fa6b97e2de2a2c74d4de9609f

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7bd2f5a9a6c60c8ae0f362b66382c4a8f986a9d8754315af98057c7cd145485
+oid sha256:bff76f76f28df79c3bb4ee5db472f53df32dc79d09f2830cfc17ca3a78384113
 size 1849
---
+++ frontend/src/pages/Mainpage.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0b715cdda4f0e6b14e90f069b655f36180f48203dd42abc4243a8b31e0f0460
+size 15969
