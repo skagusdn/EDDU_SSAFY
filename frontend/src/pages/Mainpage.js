c8288a40ac3eba1f56359f6030b2b5fccfe7df37
최영선 <vnddns26@naver.com>
2022-08-16 20:21:21 +0900

    router 수정
    Former-commit-id: 0c507e3f230ab86ad5174e3d8331cc8a0c7d8a14

--- frontend/src/pages/Mainpage.js
+++ frontend/src/pages/Mainpage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b2a1b2232babdafa9045e745acf56b4e0db756ed0de0dc8f27c53e51fe625cd
-size 16873
+oid sha256:55abf2755454d80557149f0028dbd6f6bb3a8bad91def8764507814cbc277c51
+size 17289
