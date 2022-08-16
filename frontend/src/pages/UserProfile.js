200be290bd29c63352b969ec0a8f3035de0ff926
최영선 <vnddns26@naver.com>
2022-08-16 20:40:06 +0900

    console 지우기
    Former-commit-id: 8754792ffa7349cb2c779da1ab0a1121852edee6

--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f0aedf3bd5fe8b833ba1e7da7233090ea1e24841a27198d5057d10c25290786
-size 5194
+oid sha256:0c21318989dce7018c11ba7ca2d1f74663baf008b325fb65af897edd6186f8e4
+size 5081
