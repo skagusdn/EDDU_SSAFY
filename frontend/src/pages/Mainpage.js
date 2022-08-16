fd9b77d7a43bb427a6a52016fc5be9aca7a650e6
ksmhero <ksmhero@naver.com>
2022-08-16 23:24:42 +0900

    mainpage UI finish
    Former-commit-id: ba33e538bc25fee3f1df94918b76132250275fa9

--- frontend/src/pages/Mainpage.js
+++ frontend/src/pages/Mainpage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f9d7dc7aa45fda447b1411ad99ed89fc6a6edfd65808d6e24e6a28103eca53c
-size 15831
+oid sha256:954a7e034727dbfb90f62289d4b1bae4c503744221e67775c73e55f71585e69f
+size 16074
