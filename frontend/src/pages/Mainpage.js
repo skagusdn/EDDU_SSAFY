223092f64b783a9af0b8c51885547d916e231f75
ksmhero <ksmhero@naver.com>
2022-08-16 21:23:48 +0900

    mainpage design
    Former-commit-id: ae4b5f6e2d0b0bde410b4f159de47baf7f21d5f3

--- frontend/src/pages/Mainpage.js
+++ frontend/src/pages/Mainpage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55abf2755454d80557149f0028dbd6f6bb3a8bad91def8764507814cbc277c51
-size 17289
+oid sha256:0f9d7dc7aa45fda447b1411ad99ed89fc6a6edfd65808d6e24e6a28103eca53c
+size 15831
