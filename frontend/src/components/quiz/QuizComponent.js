08235a9fced720ffed9be7a0fd0ba7a043b27141
최영선 <vnddns26@naver.com>
2022-08-16 15:34:03 +0900

    개인정보 수정 수정
    Former-commit-id: 447d20854b1acc497f6834aa2c68f2ce6a996d2e

--- frontend/src/components/quiz/QuizComponent.js
+++ frontend/src/components/quiz/QuizComponent.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2f787357c3361aaff1cc2fc4fb48959fdec3fa8da880103e336a607c01154f9
-size 13806
+oid sha256:64b16c990cb9b4558c92e7e028163aa3f7c2c0696658d117be90855d9558c3bd
+size 13782
--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99f12c15a56d819c143f403c8cb488688af22fa250a49d00fdb8ca3230a95dd8
-size 5191
+oid sha256:833da6a9c0e182f0503baafaf569e9b443e13e4da2d98c7efd9305aa1e4ee406
+size 5321
