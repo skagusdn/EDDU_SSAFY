c968205e90c3b52b7996adff05cfd64672dfa0e6
frozen_load7 <frozen_load7@naver.com>
2022-08-16 16:41:52 +0900

    room 비밀번호 오류
    Former-commit-id: 77428b0d885afb78eb53a0d5f5eac20a5ea58122

--- backend-java/src/main/java/com/ssafy/api/response/RoomRes.java
+++ backend-java/src/main/java/com/ssafy/api/response/RoomRes.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e20ad8f7aab6353fbd81983eb303207adfd74eda3162ea27818df6953a7d7b5b
+oid sha256:4d96de9949763e23f908f25c4e4594e98fccbe3e71880c4f3250a4e4d80ecab3
 size 879
