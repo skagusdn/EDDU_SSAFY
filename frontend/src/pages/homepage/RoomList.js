20f0ae6260d98579d80e21936008a0ec37f7fb2a
최영선 <vnddns26@naver.com>
2022-08-17 10:55:52 +0900

    merge
    Former-commit-id: 78b1b4c91418f27a759857399ac021e876630b83

--- frontend/src/pages/homepage/RoomList.js
+++ frontend/src/pages/homepage/RoomList.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dde90be841f10b4cf4bda3d342af9a3061eec64b97b73ea64e6dd93415ce7fb
-size 10155
+oid sha256:57a25856792454b6304ad5cb547923a68a565d5d6cdcdbaebb94a286448f7fdd
+size 8621
