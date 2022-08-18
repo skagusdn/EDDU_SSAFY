b422ad190995903ff3533cc001e7769bad4a1c23
Kim-Ji-soo <67624305+Kim-Ji-soo@users.noreply.github.com>
2022-08-18 13:54:36 +0900

    메인페이지 수정
    Former-commit-id: 69d5553d90f030082af88257f0b6e9214f628f29

--- frontend/src/pages/Mainpage.js
+++ frontend/src/pages/Mainpage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24865da4e5d59170e981cc7d999cb8317e4246b923fd23070163f5a071bf92a2
-size 16074
+oid sha256:411eee3aa2edfb9a27d0aec7c06f6fb28dd57b8f35cd6b35b5796eb7a14ba155
+size 16125
